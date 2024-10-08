1a4b2db40bbe202d4659b6bfde6a184c4e1453ea
Sharguidev <guillermoj.obando@gmail.com>
2024-10-08 12:15:10 -0600

    Correccion de Estilos login

--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -1,27 +1,91 @@
-import React from 'react';
-
-
+import React from "react";
+import "./../../styles/login.css";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-    return (
-        <>
-            <div className="container mt-5">
-                <p className="text-center mt-5"><strong>Login</strong></p>
-                <form>
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1 className="fw-bold py-5">Bienvenido al sistema!</h1>
 
-                    <div className="mb-3 ">
-                        <label for="exampleInputEmail" className="form-label mt-5">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            {/* LOGIN */}
+            <form>
+              <div class="form-floating mb-3">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="floatingInput"
+                  placeholder="name@example.com"
+                  required
+                />
+                <label for="floatingInput">Email address</label>
+              </div>
+              <div class="form-floating">
+                <input
+                  type="password"
+                  class="form-control"
+                  id="floatingPassword"
+                  placeholder="Password"
+                  required
+                />
+                <label for="floatingPassword">Password</label>
+              </div>
 
-                    </div>
-                    <div className="mb-3">
-                        <label for="exampleInputPassword1" className="form-label mt-4">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" />
-                    </div>
+              <div>
+                <button type="button" className="submit">
+                  Log In
+                </button>
+              </div>
 
-                    <button className="login">Iniciar Sesion</button>
-                </form>
-            </div>
-        </>
-    );
+              <div className="link-register mt-4 ">
+                <Link to="/register">
+                  <span></span>
+                  <a className="link-register-bground">
+                    No tienes cuenta? Registrate
+                  </a>
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+        {/* <div className="login">
+          <h1 className="form-title fw-bold">Bienvenido al sistema!</h1>
+          <div class="form-floating mb-3">
+            <input
+              type="email"
+              class="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+              required
+            />
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating">
+            <input
+              type="password"
+              class="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+              required
+            />
+            <label for="floatingPassword">Password</label>
+          </div>
+          <div>
+            <button type="button" className="submit">
+              Log In
+            </button>
+          </div>
+          <div className="link-register mt-4">
+            <Link to="/register">
+              <a className="link-register-bground">
+                No tienes cuenta? Registrate
+              </a>
+            </Link>
+          </div>
+        </div> */}
+      </div>
+    </>
+  );
 }
