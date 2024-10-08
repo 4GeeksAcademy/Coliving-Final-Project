d8c3e77679758ea7b62512187020dccd522fe5cf
Sharguidev <guillermoj.obando@gmail.com>
2024-10-08 09:21:43 -0600

    Removidas las importaciones y corregido el renderizado

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -6,9 +6,7 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Publish from "./pages/formAD.jsx";
-import Login from "./pages/login.jsx";
 import Layout from "./layout.js";
 
 //render your react application
-ReactDOM.render(<publis />, document.querySelector("#app"));
+ReactDOM.render(<Layout />, document.querySelector("#app"));
