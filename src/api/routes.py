5fb6ca5bfa20b4ba19fefa56e9666f566130cd2a
Sharguidev <guillermoj.obando@gmail.com>
2024-10-09 14:11:59 -0600

    modifying the login endpoint

--- src/api/routes.py
+++ src/api/routes.py
@@ -24,13 +24,30 @@ def handle_hello():
     return jsonify(response_body), 200
 
 
+# Login ENDPOINT
+
 @api.route("/login", methods=["POST"])
 def login():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
 
-    if email != "i5z0I@example.com" or password != "secret":
-        return jsonify({"msg": "Bad username or password"}), 401
+    if email == None or password == None:
+        return jsonify({"msg": "Houston we've got a problem, it seems that you forgot to enter your email or password"}), 401
+
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({"msg": "We couldn't find your username, but we've got a feeling it's out there having a good time. Join us and create your own adventure!"}), 404
+
+    if user.password != password:
+        return jsonify({"msg": "This key (password) doesn't open the door, please try again"}), 401
     
     acces_token = create_access_token(identity=email)
-    return jsonify(access_token=acces_token), 200
+
+    return jsonify({
+        "token": acces_token,
+        "user": user.serialize()
+    }), 200
+        
+ 
+
