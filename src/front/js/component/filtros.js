00c6107c7206c4af72dd7308e0b056e7f0fedb46
EuclidesT <94712951+EuclidesT@users.noreply.github.com>
2024-10-11 20:43:09 +0000

    En Proceso

--- src/front/js/component/filtros.js
+++ src/front/js/component/filtros.js
@@ -1,61 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Filtros = () => {
 
+  const [selectedType, selSelectedType] = useState(null);
+
+  const handleSelection = (type) => {
+    selSelectedType(type);
+  };
+
   return (
 
-    <div className="container mt-5 col-6 mx-auto">
-      <div className="d-flex justify-content-between align-items-center my-2 mx-2">
-        <i class="fa-solid fa-x"></i>
-        <h3>Filtros</h3>
+    <div className="container mt-5 col-6 mx-auto p-3">
+      <div className="d-flex justify-content-center align-items-center my-2 mx-2 position-relative">
+        <h3 className="mx-auto">Filtros</h3>
+        <i className="fa-solid fa-x position-absolute start-0 ms-2"></i>
       </div>
-      <h3 className="my-3">Recomendado para ti</h3>
-      <div className="card-group">
-        <div className="card">
-          <img src="https://images.unsplash.com/photo-1517547093881-96754b9f8f79?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="Estacionamiento Gratuito" />
-          <div className="card-body">
-            <p className="caed=text">Estacionamiento Gratuito</p>
+
+      <hr class="mt-3" />
+
+      <h3 className="my-4">Recomendado para ti</h3>
+      <div className="d-flex justify-content-center">
+        <div className="card-group gap-3">
+          <div className="card text-center border-0 bg-transparent">
+            <div className="card-body">
+              <i className="fas fa-parking fa-3x border p-3 rounded-circle"></i>
+              <p className="card=text mt-3">Estacionamiento Gratuito</p>
+            </div>
           </div>
-        </div>
-        <div className="card">
-          <img src="https://plus.unsplash.com/premium_photo-1676422355555-50d239f9d801?q=80&w=1508&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="Cancelacion Gratuita" />
-          <div className="card-body">
-            <p className="card-text">Cancelacion Gratuita</p>
+
+          <div className="card text-center border-0 bg-transparent">
+            <div className="card-body">
+              <i className="fas fa-ban fa-3x border p-3 rounded-circle"></i>
+              <p className="card-text mt-3">Cancelacion Gratuita</p>
+            </div>
           </div>
-        </div>
-        <div className="card">
-          <img src="https://plus.unsplash.com/premium_photo-1661541267138-de69ec33e540?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="Aire Acondicionado" />
-          <div className="card-body">
-            <p className="card-text">Aire Acondicionado</p>
+          <div className="card text-center border-0 bg-transparent">
+            <div className="card-body">
+              <i className="fas fa-wind fa-3x border p-3 rounded-circle"></i>
+              <p className="card-text mt-3">Aire Acondicionado</p>
+            </div>
           </div>
-        </div>
-        <div className="card">
-          <img src="https://images.unsplash.com/photo-1604335399105-a0c585fd81a1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="Lavadora" />
-          <div className="card-body">
-            <p className="card-text">Lavadora</p>
+          <div className="card text-center border-0 bg-transparent">
+            <div className="card-body">
+              <i className="fas fa-tint fa-3x border p-3 rounded-circle"></i>
+              <p className="card-text mt-3">Lavadora</p>
+            </div>
           </div>
         </div>
       </div>
+
+      <hr class="mt-3" />
 
       <h2 className="my-4">Tipo de Alojamiento</h2>
 
       <div className="btn-group" role="group" aria-label="Basic checkbox toggle button group">
-        <input type="checkbox" className="btn-check" id="btncheck1" autocomplete="off" />
-        <label className="btn btn-outline-primary" for="btncheck1">Cualquier Tipo</label>
+        <input type="checkbox" className="btn-check" id="btncheck1" autocomplete="off" onChange={() => handleSelection('any')} checked={selectedType === 'any'} />
+        <label className={"btn btn-outline-Success ${selectedType === 'any' ? 'active' : ''}"} htmlFor="btncheck1">Cualquier Tipo</label>
 
-        <input type="checkbox" className="btn-check" id="btncheck2" autocomplete="off" />
-        <label className="btn btn-outline-primary" for="btncheck2">Habitacion</label>
+        <input type="checkbox" className="btn-check" id="btncheck2" autocomplete="off" onChange={() => handleSelection('room')} checked={selectedType === 'room'} />
+        <label className={"btn btn-outline-Success ${selectedType === 'room' ? 'active' : ''}"} htmlFor="btncheck2">Habitacion</label>
 
-        <input type="checkbox" className="btn-check" id="btncheck3" autocomplete="off" />
-        <label className="btn btn-outline-primary" for="btncheck3">alojamiento entero</label>
+        <input type="checkbox" className="btn-check" id="btncheck3" autocomplete="off" onChange={() => handleSelection('entire')} checked={selectedType === 'entire'} />
+        <label className={"btn btn-outline-Success ${selectedType === 'entire' ? 'active' : ''}"} htmlFor="btncheck3">alojamiento entero</label>
       </div>
+
+      <hr class="mt-3" />
 
       <h2 className="my-4">Rango de Precios</h2>
       <p>Precios por noches sin incluir impuestos y tarifas</p>
-      <div className="d-flex justify-content-between">
-        <button type="button" class="btn btn-primary">Light</button>
-        <button type="button" class="btn btn-primary">Light</button>
+      <div className="d-flex justify-content-between gap-5">
+        <button className="btn btn-outline-secondary btn-sm" type="button">$5200</button>
+        <button class="btn btn-outline-secondary btn-sm" type="button">$190000+</button>
       </div>
+
+      <hr class="mt-3" />
+
+      <h2 className="my-4">Habitaciones y Camas</h2>
+      <div className="d-flex justify-content-between m-5">
+        <p>Habitaciones</p>
+        <p>Cualquiera</p>
+      </div>
+      <div className="d-flex justify-content-between m-5">
+        <p>Camas</p>
+        <p>Cualquiera</p>
+      </div>
+      <div className="d-flex justify-content-between m-5">
+        <p>Banos</p>
+        <p>Cualquiera</p>
+      </div>
+
+      <hr class="mt-3" />
 
     </div>
   );
