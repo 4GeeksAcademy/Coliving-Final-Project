e36d5eaab7b0cd26b15cf77cc3de4590192997f8
EuclidesT <94712951+EuclidesT@users.noreply.github.com>
2024-10-21 19:36:48 +0000

    filtros y perfil

--- src/front/js/component/Perfil.js
+++ src/front/js/component/Perfil.js
@@ -5,23 +5,23 @@ const Perfil = () => {
   const user = {
     imageUrl: "https://i.pravatar.cc/300",
     nombre: "Ramon Duarte",
-    email: "@xxxxxxx",
-    telefono: "+58 ***_***8979",
-    documento: "***********",
-    direccion: "***********",
-    contacto: "+58 ****_****90857"
+    email: "ramon@example,com",
+    telefono: "+58 1234 8979",
+    documento: "V-392834729",
+    direccion: "Calle Real 4321, Caracas",
+    contacto: "+58 7873 0857"
   }
 
 
   return (
-    <form className="container mt-5 col-6 mx-auto">
+    <form className="container mt-5 col-6 mx-auto" style={{ boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', padding: '20px', borderRadius: '10px', backgroundColor: '#fff' }} >
       <fieldset disabled>
-        <legend>Perfil de Usuario</legend>
+        <legend style={{ textAlign: 'center', marginBottom: '20px' }}>Perfil de Usuario</legend>
         <div className="mb-3 d-flex justify-content-center">
-          <img className="rounded-circle img-fluid" src={user.imageUrl} alt='Foto' style={{width: '130px', height: '130px'}} />
+          <img className="rounded-circle img-fluid" src={user.imageUrl} alt='Foto' style={{width: '130px', height: '130px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2'}} />
         </div>
         <div className="mb-3">
-          <label for="disabledTextInput" className="form-label">
+          <label htmlfor="disabledTextInput" className="form-label">
             Nombre Completo
           </label>
           <input
@@ -29,10 +29,11 @@ const Perfil = () => {
             id="disabledTextInput"
             className="form-control"
             placeholder={user.nombre}
+            // style={{ border: '1px solid #ced4da', borderRadius: '4px' }}
           />
         </div>
         <div className="mb-3">
-          <label for="disabledTextInput" className="form-label">
+          <label htmlfor="disabledTextInput" className="form-label">
             Email
           </label>
           <input
@@ -40,10 +41,11 @@ const Perfil = () => {
             id="disabledTextInput"
             className="form-control"
             placeholder={user.email}
+            // style={{ border: '1px solid #ced4da', borderRadius: '4px' }}
           />
         </div>
         <div className="mb-3">
-          <label for="disabledTextInput" className="form-label">
+          <label htmlfor="disabledTextInput" className="form-label">
             Telefono
           </label>
           <input
@@ -51,10 +53,11 @@ const Perfil = () => {
             id="disabledTextInput"
             className="form-control"
             placeholder={user.telefono}
+            // style={{ border: '1px solid #ced4da', borderRadius: '4px' }}
           />
         </div>
         <div className="mb-3">
-          <label for="disabledTextInput" className="form-label">
+          <label htmlfor="disabledTextInput" className="form-label">
             Documento de indentidad
           </label>
           <input
@@ -62,10 +65,11 @@ const Perfil = () => {
             id="disabledTextInput"
             className="form-control"
             placeholder={user.documento}
+            // style={{ border: '1px solid #ced4da', borderRadius: '4px' }}
           />
         </div>
         <div className="mb-3">
-          <label for="disabledTextInput" className="form-label">
+          <label htmlfor="disabledTextInput" className="form-label">
             Direccion
           </label>
           <input
@@ -73,10 +77,11 @@ const Perfil = () => {
             id="disabledTextInput"
             className="form-control"
             placeholder={user.direccion}
+            // style={{ border: '1px solid #ced4da', borderRadius: '4px' }}
           />
         </div>
         <div className="mb-3">
-          <label for="disabledTextInput" className="form-label">
+          <label htmlfor="disabledTextInput" className="form-label">
             Contacto de Emergencia
           </label>
           <input
@@ -84,11 +89,14 @@ const Perfil = () => {
             id="disabledTextInput"
             className="form-control"
             placeholder={user.contacto}
+            // style={{ border: '1px solid #ced4da', borderRadius: '4px' }}
           />
         </div>
-        <button type="submit" className='button-centered'>
-          Editar
-        </button>
+        <div className="text-center">
+          <button type="submit" className="btn btn-primary" style={{ backgroundColor: '#007bff', borderColor: '#007bff', borderRadius: '50px', padding: '10px 20px' }}>
+            Editar
+          </button>
+        </div>
       </fieldset>
     </form>
   );
