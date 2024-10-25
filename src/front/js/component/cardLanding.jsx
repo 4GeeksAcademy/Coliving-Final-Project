b09d9572dfd60b687940f9845cacd023eb076708
Andy01hb <128887067+Andy01hb@users.noreply.github.com>
2024-10-25 01:50:48 +0000

    revertí cambios que hice en un componente ajeno

--- src/front/js/component/cardLanding.jsx
+++ src/front/js/component/cardLanding.jsx
@@ -3,46 +3,39 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 const CardLanding = ({ property }) => {
-  const images = property.files ? property.files.split(",") : [];
-  const mainImage = images.length > 0 ? images[0] : "default-image.jpg"; // Imagen por defecto si no hay archivos.
-
   return (
-    <Link to={`/hostProperty/${property.id}`} className="text-decoration-none text-black">
-      <div>
-        <div className="card shadow-sm">
-          <img src={mainImage} className="card-img-top" alt="Imagen de la propiedad" />
-          <div className="card-body">
-            <h5 className="card-title">{property.name}</h5>
-            <p className="card-text">{property.description}</p>
-            <p>{property.rooms_number} habitaciones - {property.beds} camas - {property.restrooms} baños</p>
-            <p><strong>${property.price}</strong></p>
+    <>
+      <Link to={'/single/' + property.id} className="text-decoration-none text-black">
+        <div>
+          <div className="card shadow-sm">
+            <img src={property.files} className="card-img-top" alt="Imagen de la propiedad" />
+            <div className="card-body">
+              <h5 className="card-title">{property.name}</h5>
+              <p className="card-text">{property.description}</p>
+              <p>{property.rooms_number} habitaciones - {property.beds} camas - {property.restrooms} baños</p>
+              <p><strong>${property.price}</strong></p>
+            </div>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </>
   );
-}
+};
 
 const CardsLanding = () => {
   const { store } = useContext(Context);
-
+  
   return (
-    <div className="m-5">
-      <div className="card-container row">
-        {
-          store.properties.length > 0 ? (
-            store.properties.map((item, index) => (
-              <div key={index} className="col-md-4 mb-4">
-                <CardLanding property={item} />
-              </div>
-            ))
-          ) : (
-            <p>No hay propiedades disponibles en este momento.</p>
-          )
-        }
+    <>
+      <div className="m-5">
+        <div className="card-container">
+          {store.properties.map((item, index) => {
+            return <CardLanding property={item} key={index} />;
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
-}
+};
 
 export default CardsLanding;
