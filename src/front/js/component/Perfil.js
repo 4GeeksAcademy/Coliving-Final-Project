9801bccc4e53dd3e24a7807f44826eab79386f6f
EuclidesT <94712951+EuclidesT@users.noreply.github.com>
2024-10-25 21:14:36 +0000

    Perfil

--- src/front/js/component/Perfil.js
+++ src/front/js/component/Perfil.js
@@ -1,37 +1,59 @@
-import React from 'react';
-
+import React, {useEffect, useContext, useState} from 'react';
+import {Context} from "../store/appContext";
 
 const Perfil = () => {
-  const user = {
-    imageUrl: "https://i.pravatar.cc/300",
-    nombre: "Ramon",
-    apellido:"Duarte",
-    email: "ramon@example.com",
-    telefono: "+58 1234 8979",
-    documento: "V-392834729",
-    direccion: "Calle Real 4321, Caracas",
-    contacto: "+58 7873 0857"
-  }
+  const {store, actions}=useContext( Context )
+  const [user, setUser]=useState({})
+  
 
+useEffect(() => {
+  obtenerPerfil()
+},[])
+
+const obtenerPerfil=async() => {
+  let resp=await actions.getUserLogged()
+  if(resp){
+    const initialValues = {
+      imageUrl: "https://i.pravatar.cc/300",
+      nombre: store.user.first_name,
+      apellido:store.user.last_name,
+      email: store.user.email,
+      telefono: store.user.phone,
+      documento: store.user.identity_document,
+      direccion: store.user.address,
+      contacto: store.user.emergency_phone
+    }
+    setUser(initialValues)
+  }
+}
+
+const handleChange=(e) => {
+  setUser({
+    ...user,
+    [e.target.name]:e.target.value
+  })
+}
 
   return (
     <form className="container mt-5 col-6 mx-auto" style={{ boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', padding: '20px', borderRadius: '10px', backgroundColor: '#fff' }} >
-      <fieldset disabled>
+      <fieldset>
         <legend style={{ textAlign: 'center', marginBottom: '20px' }}>Perfil de Usuario</legend>
         <div className="mb-3 d-flex justify-content-center">
           <img className="rounded-circle img-fluid" src={user.imageUrl} alt='Foto' style={{ width: '130px', height: '130px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2' }} />
         </div>
         <div className="mb-3">
-          <label htmlfor="disabledTextInput" className="form-label">
+          <label htmlfor="" className="form-label">
             Nombre Completo
           </label>
           <div className='row'>
             <div className='col'>
               <input
                 type="text"
-                id="disabledTextInput"
+                // id="disabledTextInput"
                 className="form-control"
-                placeholder={user.nombre}
+                value={user.nombre}
+                onChange={(e)=> handleChange(e)}
+                name="nombre"
               // style={{ border: '1px solid #ced4da', borderRadius: '4px' }}
               />
             </div>
@@ -40,7 +62,9 @@ const Perfil = () => {
                 type="text"
                 id="disabledTextInput"
                 className="form-control"
-                placeholder={user.apellido}
+                value={user.apellido}
+                onChange={(e)=> handleChange(e)}
+                name="apellido"
               // style={{ border: '1px solid #ced4da', borderRadius: '4px' }}
               />
             </div>
@@ -54,7 +78,9 @@ const Perfil = () => {
             type="text"
             id="disabledTextInput"
             className="form-control"
-            placeholder={user.email}
+            value={user.email}
+            onChange={(e)=> handleChange(e)}
+            name="email"
           // style={{ border: '1px solid #ced4da', borderRadius: '4px' }}
           />
         </div>
@@ -66,19 +92,23 @@ const Perfil = () => {
             type="text"
             id="disabledTextInput"
             className="form-control"
-            placeholder={user.telefono}
+            value={user.telefono}
+                onChange={(e)=> handleChange(e)}
+                name="telefono"
           // style={{ border: '1px solid #ced4da', borderRadius: '4px' }}
           />
         </div>
         <div className="mb-3">
           <label htmlfor="disabledTextInput" className="form-label">
-            Documento de indentidad
+            Documento de identidad
           </label>
           <input
             type="text"
             id="disabledTextInput"
             className="form-control"
-            placeholder={user.documento}
+            value={user.documento}
+                onChange={(e)=> handleChange(e)}
+                name="documento"
           // style={{ border: '1px solid #ced4da', borderRadius: '4px' }}
           />
         </div>
@@ -90,7 +120,9 @@ const Perfil = () => {
             type="text"
             id="disabledTextInput"
             className="form-control"
-            placeholder={user.direccion}
+            value={user.direccion}
+                onChange={(e)=> handleChange(e)}
+                name="direccion"
           // style={{ border: '1px solid #ced4da', borderRadius: '4px' }}
           />
         </div>
@@ -102,7 +134,9 @@ const Perfil = () => {
             type="text"
             id="disabledTextInput"
             className="form-control"
-            placeholder={user.contacto}
+            value={user.contacto}
+                onChange={(e)=> handleChange(e)}
+                name="contacto"
           // style={{ border: '1px solid #ced4da', borderRadius: '4px' }}
           />
         </div>
