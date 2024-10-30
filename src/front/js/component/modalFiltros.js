2cf3823866046553301539da652de1e81abe050e
Anguibell <romeroanguibell17@gmail.com>
2024-10-30 02:54:46 +0000

    Ultimos cambios

--- src/front/js/component/modalFiltros.js
+++ src/front/js/component/modalFiltros.js
@@ -1,10 +1,6 @@
 import React, { useState } from 'react';
 import Filtros from './filtros';
-// import { BtnFiltros } from './filtros';
 import "./../../styles/filtros.css";
-
-
-
 
 const Modal = () => {
 
@@ -12,12 +8,10 @@ const Modal = () => {
 
     return (
         <>
-
-            <button type="button" className="btn btn-danger ms-5 btn-filtros" data-bs-toggle="modal" data-bs-target="#exampleModal"
-                onClick={() => setIsOpen(true)}
-            >
-                Aplicar filtros <i class="fa-solid fa-caret-down d-flex d-inline-flex"></i>
-            </button>
+            <div className="container d-flex justify-content-end">
+                <button className="m-0 mt-5" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                    onClick={() => setIsOpen(true)}>Aplicar filtros <i class="fa-solid fa-caret-down d-flex d-inline-flex"></i></button>
+            </div>
 
             <div className="modal fade modal-xl" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div className="modal-dialog">
@@ -29,21 +23,11 @@ const Modal = () => {
                         <div className="modal-body">
                             <Filtros />
                         </div>
-
                     </div>
                 </div>
             </div>
-
-
-
-
-
-
         </>
     );
 };
 
-
 export default Modal;
-
-
